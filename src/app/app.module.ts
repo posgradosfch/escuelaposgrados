08c1a71a76e5d50cf888946f082711ef99a9d759
marisolgarcia <egarcia.abrego@gmail.com>
2018-06-14 21:05:41 -0600

    login parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
+import { FormsModule} from '@angular/forms';
+
+import { HomeComponent } from './home/home.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { OfertaComponent } from './oferta/oferta.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: 'noticias', component: NoticiasComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuienesSomosComponent,
+    OfertaComponent,
+    NoticiasComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
