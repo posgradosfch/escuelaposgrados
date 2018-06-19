0e4497c3f2d1e173606dd285fc17239e405a59e3
marisolgarcia <egarcia.abrego@gmail.com>
2018-06-18 19:20:53 -0600

    ultimos cambios

--- src/app/carrusel/carrusel.component.ts
+++ src/app/carrusel/carrusel.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
-  styleUrls: ['./carrusel.component.css']
+  styleUrls: ['./carrusel.component.css'],
 })
 export class CarruselComponent implements OnInit {
 
